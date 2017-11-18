8e9f1863e094eb0a211c43b023507a3c3f431327
HabibApez <habibapez@hotmail.com>
2017-11-17 19:27:15 -0600

    Version 3

--- lib/APP/Scheduler/Main.c
+++ lib/APP/Scheduler/Main.c
@@ -1,30 +1,101 @@
-/*
- * Main implementation: use this 'C' sample to create your own application
- *
+/*============================================================================*/
+/*                        I BS SOFTWARE GROUP                                 */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*/
+/*!
+ * $Source: Main.c $
+ * $Revision: version 1$
+ * $Author: Habib Apez $
+ * $Date: 2017-11- 16 $
  */
+/*============================================================================*/
+/* DESCRIPTION :                                                              */
+/** \Mainc
+    Main at APP in Scheduler.
+    Binary Progression Scheduler.
+*/
+/*============================================================================*/
+/* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
+/* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
+/* ALL RIGHTS RESERVED                                                        */
+/*                                                                            */
+/* The reproduction, transmission, or use of this document or its content is  */
+/* not permitted without express written authority. Offenders will be liable  */
+/* for damages.                                                               */
+/* All rights, including rights created by patent grant or registration of a  */
+/* utility model or design, are reserved.                                     */
+/*                                                                            */
+/*============================================================================*/
+/*============================================================================*/
+/*                    REUSE HISTORY - taken over from                         */
+/*============================================================================*/
+/*  Author             |        Version     | FILE VERSION (AND INSTANCE)     */
+/*----------------------------------------------------------------------------*/
+/* Habib Apez          |          1         |   Initial version               */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*
+ * $Log: Main.c  $
+  ============================================================================*/
 
-#include "Std_Types.h"
-#include "Port.h"
-#include "Wdog.h"
-#include "Nvic.h"
-#include "Mcu.h"
-#include "SchM.h"
+/* Includes */
+/*============================================================================*/
+#include "C:\My Documents\Continental\IAR\Devs\lib\Common\Std_Types.h"
+#include "C:\My Documents\Continental\IAR\Devs\lib\HAL\clock.c"
+#include "C:\My Documents\Continental\IAR\Devs\lib\HAL\delays.c"
+#include "C:\My Documents\Continental\IAR\Devs\lib\HAL\button.c"
+#include "C:\My Documents\Continental\IAR\Devs\lib\HAL\segmentbar.c"
+#include "C:\My Documents\Continental\IAR\Devs\lib\HAL\leds.c"
+#include "C:\My Documents\Continental\IAR\Devs\lib\SERVICES\Interrupts\interrupts.c"
+#include "C:\My Documents\Continental\IAR\Devs\lib\SERVICES\Scheduler\SchM.c"
+#include "C:\My Documents\Continental\IAR\Devs\lib\SERVICES\Scheduler\SchM_Cfg.c"
 
-int main(void)
-{
-	WDOG_disable();
-	PORT_init();             				/* Configure ports */
-	SOSC_init_8MHz();        				/* Initialize system oscilator for 8 MHz xtal */
-	SPLL_init_160MHz();      				/* Initialize SPLL to 160 MHz with 8 MHz SOSC */
-	NormalRUNmode_80MHz();   				/* Init clocks: 80 MHz sysclk & core, 40 MHz bus, 20 MHz flash */
-	NVIC_init_IRQs();        				/* Enable desired interrupts and priorities */
-	SchM_Init(&SchedulerConfig);			/* Scheduler Services Initialization */
-	SchM_Start();							/* Start Scheduler Services */
+/* Constants and types  */
+/*============================================================================*/
 
-	/* Further code should not be reached */
-	for(;;) {
+/* Variables */
+/*============================================================================*/
 
-	}
+/* Private functions prototypes */
+/*============================================================================*/
+void SysTick_Handler(void);
 
-	return (0);
+/* Inline functions */
+/*============================================================================*/
+
+/* Private functions */
+/*============================================================================*/
+/**************************************************************
+ *  Name                 : SystTick interruption
+ *  Description          : Moves the Window upwards
+ *  Parameters           : [void]
+ *  Return               : void
+ *  Critical/explanation : No
+ **************************************************************/
+void SysTick_Handler(void){
+  if ( NULL!= GlbSysTickCallback)
+	  GlbSysTickCallback();
+  leds_ToggleBlueBoardLED();
 }
+
+ void main(void){
+  clock_InitClock();
+  delays_InitTimer();
+  segmentbar_InitBar();
+  leds_InitBoardLeds();
+  leds_InitLeds();
+    
+  SchM_Init(&SchM_Config);			/* Scheduler Services Initialization */
+  SchM_Start();							/* Start Scheduler Services */
+    
+  for(;;){
+    // Do nothing
+  }
+}
+
+/* Exported functions */
+/*============================================================================*/
+
+ /* Notice: the file ends with a blank new line to avoid compiler warnings */
